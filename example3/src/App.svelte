42dc909fbe77d8a8b4f16d88b23ec57f4b145452
Robert.Yang <ysb05222@naver.com>
2021-07-03 10:20:17 +0900

    조건문과 반복문
    조건문과 반복문

---
+++ example3/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	let name = 'world';
+	let isShow = false;
+	
+	function toggle() {
+		isShow = !isShow
+	}
+</script>
+
+<button on:click={toggle}>
+	Click!
+</button>
+{#if isShow}
+	<h1>Hello {name}!</h1>
+{:else if name === 'world'}
+	<h3>
+		Svelte	
+	</h3>
+{:else}
+	<h2>
+		Sungbin!	
+	</h2>
+{/if}
---
+++ example4/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'];
+	
+	function deleteFruit() {
+		fruits.splice(2, 1)
+		fruits = fruits
+	}
+</script>
+
+<button on:click={deleteFruit}>
+	Click!
+</button>
+<ul>
+	{#each fruits as fruit (fruit)}
+			<li>{fruit}</li>
+	{/each}
+</ul>
