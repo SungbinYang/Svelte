16d75da3920737ef5b085fa0114a3db097bf6c41
Robert.Yang <ysb05222@naver.com>
2021-07-03 11:36:55 +0900

    스토어
    스토어

---
+++ example7/src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Parent from './Parent.svelte'
+	import {name} from './store.js'
+</script>
+
+<h1>Hello {$name}!</h1>
+<Parent />
---
+++ example7/src/Child.svelte
@@ -0,0 +1,7 @@
+<script>
+	import {name} from './store.js'
+</script>
+
+<h2 on:click={() => {$name = 'Child'}}>
+	Child!
+</h2>
