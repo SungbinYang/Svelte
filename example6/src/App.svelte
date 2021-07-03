13650833dddbd984cf51cf345fac36d4e840f71c
Robert.Yang <ysb05222@naver.com>
2021-07-03 11:05:51 +0900

    컴포넌트
    컴포넌트

---
+++ example6/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Fruit from './Fruit.svelte';
+	let fruits = [
+		{name: 'Apple', color: 'red'},
+		{name: 'banana', color: 'yellow'},
+		{name: 'cherry', color: 'blue'},
+	  {name: 'Orange', color: 'orange'}
+	]
+</script>
+
+<ul>
+	{#each fruits as {name, color} (name)}
+		<Fruit fruit = {name} {color}/>
+	{/each}
+</ul>
---
+++ example6/src/Fruit.svelte
@@ -0,0 +1,6 @@
+<script>	
+	export let fruit
+	export let color
+</script>
+
+<li style="color: {color};">{fruit}</li>
