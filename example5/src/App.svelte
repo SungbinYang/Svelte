823c998814c999e177acf6f603ca7b4758c0f3da
Robert.Yang <ysb05222@naver.com>
2021-07-03 10:45:54 +0900

    이벤트 핸들링
    이벤트 핸들링

---
+++ example5/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	let name = 'world';
+	let isRed = false;
+</script>
+
+<h1>Hello {name}!</h1>
+<div class="box" 
+		 style="background-color: {isRed ? 'red' : 'orange'};" 
+		 on:click={() => isRed = !isRed}
+		 on:mouseenter={() => name = 'Enter!'}
+		 on:mouseleave={() => name = 'Leave!'}
+		 >
+	
+</div>
+
+<style>
+	.box {
+		width: 300px;
+		height: 150px;
+		background-color: orange
+	}
+</style>
