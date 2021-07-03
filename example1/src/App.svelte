36e4ee3a9407205f066ccad951856f2ac9740b14
Robert.Yang <ysb05222@naver.com>
2021-07-03 09:51:48 +0900

    선언적 렌더링
    선언적 렌더링

---
+++ example1/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	const a = 'ABC'
+	let name = 'world';
+	let age = 85;
+	
+	function abc() {
+		name = 'ROVERT';
+	}
+</script>
+
+<h1>Hello {name}!</h1>
+<h2>
+	{a}
+</h2>
+<button on:click={abc}>
+	Click
+</button>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
---
+++ example2/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name = 'world';
+	let isActive = false;
+	
+	function abc() {
+		name = 'ROVERT';
+		isActive = true;
+	}
+</script>
+
+<h1 class:active={isActive}>Hello {name}!</h1>
+<input type="text" bind:value={name} />
+<button on:click={abc}>
+	Click
+</button>
+
+<style>
+	h1.active {
+		color: slateBlue;
+	}
+</style>
